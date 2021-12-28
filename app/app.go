33a7c3d9ed972501b74247ec7f3ba6323803422d
Daniel Lewan <vision360.daniel@gmail.com>
2021-12-28 19:05:04 +0100

    WIP debug commands

--- app/app.go
+++ app/app.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"fmt"
+	"os/exec"
+	"regexp"
+
 	"github.com/TeddyDD/kakoune-wiki/domain/kakoune"
 	"github.com/TeddyDD/kakoune-wiki/domain/wiki"
 )
@@ -32,4 +36,36 @@ func New(
 			return wiki.Files()
 		},
 	}
+}
+
+var singleQuote = regexp.MustCompile(`'`)
+
+func (a app) Debug(msg string) {
+	if a.config == nil || !a.config.Debug {
+		return
+	}
+	kak, err := exec.LookPath("kak")
+	if err != nil {
+		panic(err)
+	}
+
+	cmd := exec.Command(kak, "-p", a.config.Session)
+
+	in, err := cmd.StdinPipe()
+	if err != nil {
+		panic(err)
+	}
+	defer in.Close()
+
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	msg = singleQuote.ReplaceAllString(msg, `''`)
+
+	fmt.Fprintf(in, "echo -debug wiki: '%s'", msg)
+}
+
+func (a app) Debugf(format string, val ...interface{}) {
+	a.Debug(fmt.Sprintf(format, val...))
 }
